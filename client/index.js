f56b321601046bf1169bcaf25b5fdffef40da376
Buena Chan <buenachan@Buenas-MacBook-Pro.local>
2018-05-02 19:45:28 -0400

    first commit

---
+++ client/index.js
@@ -0,0 +1,5 @@
+import React, { Component } from 'react'
+import ReactDOM from 'react-dom'
+import Main from './components/Main.js'
+
+ReactDOM.render(<Main />, document.getElementById('app'))
---
+++ main.js
@@ -0,0 +1,34 @@
+'use strict'
+
+const {app, BrowserWindow} = require('electron')
+const path = require('path')
+const url = require('url')
+let mainWindow = null
+
+console.log('DIRNAME', path.join(__dirname, '/public/index.html'))
+
+app.on('ready', function(){
+  mainWindow = new BrowserWindow({
+    frame: false,
+    height: 700,
+    resizable: true,
+    width: 368
+  })
+  // mainWindow.loadURL('file://'+__dirname+'/app/index.html')
+  mainWindow.loadURL(url.format({
+    pathname: path.join(__dirname,'public/index.html'),
+    protocol: 'file:',
+    slashes: true
+    }));
+    mainWindow.openDevTools();
+    mainWindow.on('closed', function() {
+      mainWindow = null;
+      });
+  }
+)
+
+app.on('window-all-closed', function() {
+	if (process.platform != 'darwin') {
+	app.quit();
+  	}
+});
