d2513aa90db04882bf88f85925c1a55a0728d84a
Mightynumnums <deadcookies@gmail.com>
2018-05-03 17:32:56 -0400

    sorting merges

--- client/components/index.js
+++ client/components/index.js
@@ -1,10 +1,7 @@
 export { default as Navbar } from './navbar'
-<<<<<<< HEAD
 export { default as Donke } from './donke'
 export { default as DonkeSick } from './donkeSick'
 export { default as Heart } from './heart'
 export { default as SpeechBubble } from './speechBubble'
-=======
 export { default as SelectDonke } from './selectDonke'
-export { default as Heart } from './heart'
->>>>>>> master
+
