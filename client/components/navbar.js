60af25dd2d68ba691142612ec430d804bf02fad7
Buena Chan <buenachan@Buenas-MacBook-Pro.local>
2018-05-03 12:39:15 -0400

    updated css

--- client/components/navbar.js
+++ client/components/navbar.js
@@ -1,23 +1,31 @@
 import React, {Component} from 'react'
 
-export default (props) => {
-  return(
-    <div className="navbar">
-        <div className="navbar-work">Set Work Interval</div>
-        {/* <div className="dropdown"> */}
-          {/* <div className="dropdown-content"> */}
-        <div className="navbar-break">Set Break Interval</div>
-          <div className="break-interval">
-            <select>
-              {
-                [10,20,30].map(interval => {
-                return (
-                <option>{interval}</option>
-                )})
-              }
-            </select>
-          </div>
-    </div>
-  )
+export default class Navbar extends Component {
+  constructor(){
+    super()
+    this.state = {
+
+    }
+  }
+  render(){
+    return(
+      <div className="navbar-container">
+          <div className="navbar-work">Set Work Interval</div>
+          {/* <div className="dropdown"> */}
+            {/* <div className="dropdown-content"> */}
+          <div className="navbar-break">Set Break Interval</div>
+            <div className="break-interval">
+              <select>
+                {
+                  [10,20,30].map(interval => {
+                  return (
+                  <option>{interval}</option>
+                  )})
+                }
+              </select>
+            </div>
+      </div>
+    )
+  }
 }
 
