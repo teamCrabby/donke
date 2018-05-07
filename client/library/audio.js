fffa795c678dfaa706862c5afade29df5f923375
Buena Chan <buenachan@Buenas-MacBook-Pro.local>
2018-05-07 14:13:56 -0400

    took out nonworking sounds

--- client/library/audio.js
+++ client/library/audio.js
@@ -5,28 +5,22 @@ export const playAudio = soundName => {
 
   switch(soundName) {
     case 'happy':
-      options = ['http://www.brunover.com/wav_gif/donkey.wav','http://www.nachoua.com/Animaux/ane.wav']
+      options = ['http://www.nachoua.com/Animaux/ane.wav']
       break;
-    case 'dramatic':
-      options = ['goosie.cogsci.indiana.edu/.../donkey.wav']
-      break;
+    // case 'dramatic':
+    //   options = ['goosie.cogsci.indiana.edu/.../donkey.wav']
+    //   break;
     case 'upset':
-      options = ['images.tonebytone.com/.../donkey2.wav','http://izzyweird.com/soundlib1/donkey2.wav','www.egyptiancastle.com/.../DONKEY_1.WAV']
+      options = ['http://izzyweird.com/soundlib1/donkey2.wav', 'http://www.brunover.com/wav_gif/donkey.wav']
       break;
     case 'short':
-      options = ['schaeffer.ludo.free.fr/.../HOLYDONKEYIMPACT.WAV','www.presepioelettronico.it/.../asino.wav','http://www.davorin.net/d/osel.wav','www.egyptiancastle.com/.../DONKEY.WAV']
-      break;
-    case 'shriek':
-      options =['www.sounds.beachware.com/.../DONKEYB.mp3']
-      break;
-    case 'gasp':
-      options = ['www.cs.unc.edu/.../donkey.wav']
+      options = ['http://www.davorin.net/d/osel.wav']
       break;
     case 'heehaw':
       options = ['http://www.mountaincharlie1850.org/sounds/hee_haw_04.wav']
       break;
     default:
-      options = ['http://www.brunover.com/wav_gif/donkey.wav']   
+      options = ['http://www.nachoua.com/Animaux/ane.wav']   
   }
 
   let link = options[Math.floor(Math.random()*(options.length-1+1))]
