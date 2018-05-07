c6cc0af5b0ad59fbca57da9f2d71f562930fc28d
Christine Ernst <christineernst@Christines-MacBook-Air.local>
2018-05-07 16:46:09 -0400

    cloud work

--- client/components/cloud.js
+++ client/components/cloud.js
@@ -15,6 +15,9 @@ export default class Cloud extends Component {
       <div>
         <img id="cloud1" src="../img/cloud.svg" width="220" height="220" />
         <img id="cloud2" src="../img/cloud.svg" width="220" height="220" />
+        <img id="cloud3" src="../img/cloud.svg" width="300" height="300" />
+        <img id="cloud4" src="../img/cloud.svg" width="220" height="220" />
+        <img id="cloud5" src="../img/cloud.svg" width="220" height="220" />
       </div>
     );
   }
