28370bceec9eeae1a727943765bb0fc5cc0efa64
Mightynumnums <deadcookies@gmail.com>
2018-05-07 18:20:14 -0400

    stuff

--- client/components/donke.js
+++ client/components/donke.js
@@ -1,52 +1,28 @@
 import React, { Component } from 'react';
-import { entrance } from '../library/animations';
-import { playAudio } from '../library/audio';
-import { connect } from 'react-redux';
-import { playAudio } from '../library/audio';
-import store, { fetchHealth } from '../store';
+import { sunRotate } from '../library/animations'
+import { playAudio } from '../library/audio'
 
 
 export default class Donke extends Component {
   constructor(props) {
     super(props);
   }
-  // componentDidMount() {
-  //   entrance()
-  // }
+  componentDidMount() {
+    sunRotate();
+  }
 
 
   render() {
 
     return (
       <div className="panel">
-        <img id="animal" src="../img/donke10.svg" width="300" height="450" onClick={() => playAudio('happy')} />
+        <img id="sunFace" src="../img/sunFace.svg" />
+        <img id="sunRays" src="../img/sunRays.svg" />
+        <img id="donke" src="../img/donke10.svg" onClick={() => playAudio('happy')} />
+        <img id="grass" src="../img/grass.svg" />
       </div>
-    );
+    )
   }
 }
 
-
-const mapStateToProps = state => {
-  return {
-    workInterval: state.workInterval,
-    breakInterval: state.breakInterval,
-    health: state.health
-  }
-}
-
-const mapDispatchToProps = dispatch => {
-  return {
-    setStoreHealth(health) {
-      dispatch(fetchHealth(health))
-    }
-  }
-}
-
-export default connect(mapStateToProps, mapDispatchToProps)(SelectDonke)
-
-//map state to props
-// pull the health property
-//src path in backtics `..img/donke${this.props.health}.svg`
-//we will also need connect
-//export that shiz
 
