7fc908b3e6fc4ccf49dde55a98502ac8849635fe
Christine Ernst <christineernst@Christines-MacBook-Air.local>
2018-05-08 13:11:33 -0400

    idletime saving to the store

--- client/store/index.js
+++ client/store/index.js
@@ -1,13 +1,14 @@
-import { createStore, combineReducers, applyMiddleware } from 'redux'
-import { createLogger } from 'redux-logger'
-import thunkMiddleware from 'redux-thunk'
-import { composeWithDevTools } from 'redux-devtools-extension'
-import workInterval from './workInterval'
-import breakInterval from './breakInterval'
-import health from './health'
+import { createStore, combineReducers, applyMiddleware } from 'redux';
+import { createLogger } from 'redux-logger';
+import thunkMiddleware from 'redux-thunk';
+import { composeWithDevTools } from 'redux-devtools-extension';
+import workInterval from './workInterval';
+import breakInterval from './breakInterval';
+import health from './health';
+import idleTimeReducer from './idletime';
 
 
-const reducer = combineReducers({ workInterval, breakInterval, health })
+const reducer = combineReducers({ workInterval, breakInterval, health, idleTimeReducer })
 const middleware = composeWithDevTools(applyMiddleware(
   thunkMiddleware,
   createLogger({ collapsed: true })
