b3a8e4c7bf4e5839ffb359ce925b251e57842005
Mightynumnums <deadcookies@gmail.com>
2018-05-09 17:02:11 -0400

    trying to get db to put things into itself

--- client/app.js
+++ client/app.js
@@ -1,5 +1,21 @@
 import React, { Component } from 'react';
 import { Navbar, Heart, SpeechBubble, SelectDonke, PartyHat, HealthBar, Login } from './components'
+const firebase = require("firebase")
+const secrets = require('../secrets.js')
+require("firebase/firestore")
+
+firebase.initializeApp({
+  apiKey: secrets.API_KEY,
+  authDomain: secrets.AUTH_DOMAIN,
+  databaseURL: secrets.DATABASE_URL,
+  projectId: secrets.PROJECT_ID,
+  storageBucket: secrets.STORAGE_BUCKET,
+  messagingSenderId: secrets.MESSAGING_SENDER_ID
+});
+
+export const db = firebase.firestore();
+const settings = {/* your settings... */ timestampsInSnapshots: true };
+db.settings(settings);
 
 export default class App extends Component {
   constructor(props) {
--- client/components/playpen.js
+++ client/components/playpen.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import { db } from '../app'
 
 
 export default class Playpen extends Component {
@@ -7,25 +8,36 @@ export default class Playpen extends Component {
 
     this.state = {
       playPenName: '',
-      inviteName: ''
+      invitedUser: '',
+      users: ['trice']
     }
     this.handleChange = this.handleChange.bind(this)
     this.handleSubmit = this.handleSubmit.bind(this)
+    this.handleAddABuddy = this.handleAddABuddy.bind(this)
   }
 
-  handleSubmit(event) {
-    event.preventDefault();
-    // const newPlayPen = {}
-    //submit somewhere
-    console.log(`you picked: ${this.state.playPenName}`)
+
+  handleSubmit(event, db, playPenName, users) {
+    event.preventDefault()
+    db.collection('playPen').add({
+      name: playPenName,
+      users: ['a', 'b', 'c']
+    })
+      .then((res) => console.log(res))
+      .catch((error) => console.log(`Unable to save playpen ${error.message}`))
   }
 
-  // componentDidMount() {
-  //   this.setState({
-  //     playPenName: event.target.playPenName,
-  //     inviteName: event.target.inviteName
-  //   })
-  // }
+
+  handleAddABuddy(event, invitedUser, usersArr) {
+    console.log(invitedUser)
+    console.log('SPREADSS', usersArr)
+    event.preventDefault()
+    this.setState({
+      users: [invitedUser, ...usersArr]
+
+    })
+  }
+
 
   handleChange(event) {
     this.setState({ [event.target.name]: event.target.value })
@@ -33,27 +45,33 @@ export default class Playpen extends Component {
 
 
   render() {
+    console.log(this.state)
     return (
       <div className="formContainer">
         <div className="playPen">
-          <form onSubmit={this.handleSubmit}>
+          <form onSubmit={(event) => this.handleSubmit(event, db, this.state.playPenName, this.state.users)}>
             <label>Pick a name for your playpen: </label>
             <div>
               <input name='playPenName' type="text" value={this.state.playPenName} onChange={this.handleChange} />
             </div>
-            <button type="submit">Submit</button>
           </form>
         </div>
         <div className="pickFriend">
           <form onSubmit={this.handleSubmit}>
             <label>Who do you want to invite over?</label>
-            <select name="inviteName" onChange={this.handleChange}>
+            <select name="invitedUser" onChange={this.handleChange}>
               {
                 ['boddy', 'suzie', 'trashcan', 'poopsie', 'puberty'].map((name, idx) => {
                   return (<option key={idx}>{name}</option>)
                 })
               }
             </select>
+            <div>
+              <button onClick={(event) => this.handleAddABuddy(event, this.state.invitedUser, this.state.users)}>Add a Buddy</button>
+            </div>
+            <div>
+              <button type="submit">Submit</button>
+            </div>
           </form>
         </div>
       </div>
