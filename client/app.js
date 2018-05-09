d6298a0e7088208d0c185c77100342f48377f4ea
Christine Ernst <christineernst@Christines-MacBook-Air.local>
2018-05-09 17:02:10 -0400

    receiving notifications of changes to an avatar

--- client/app.js
+++ client/app.js
@@ -1,10 +1,25 @@
 import React, { Component } from 'react';
-import { Navbar, Heart, SpeechBubble, SelectDonke, PartyHat, HealthBar, Login } from './components'
+import { Navbar, Heart, SpeechBubble, SelectDonke, PartyHat, HealthBar, Login, Playpen } from './components';
+import firebase from 'firebase';
 
 export default class App extends Component {
   constructor(props) {
     super(props)
+    this.avatar = firebase.firestore().collection('avatars').doc('RLAstb3EigfEWlhL1I4m')
+    this.onUpdate = this.onUpdate.bind(this)
   }
+
+  componentDidMount() {
+    this.farts = this.avatar.onSnapshot(this.onUpdate);
+  }
+  componentWillUnmount() {
+    this.farts();
+  }
+
+  onUpdate (snapshot) {
+    console.log('SNAPSHOT', snapshot.data())
+  };
+
   render() {
     return (
       <div>
@@ -13,7 +28,10 @@ export default class App extends Component {
           <Login />
         </div>
         <div className="animal">
-          <SelectDonke />
+          {
+          //<SelectDonke />
+          }
+          <Playpen />
         </div>
       </div>
     )
