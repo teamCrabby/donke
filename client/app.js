07de515c01ee712fd8a9df4dd355fab1047b147f
Rachel Fine <rachelefine@Rachels-MacBook-Air.local>
2018-05-12 17:09:33 -0400

    took out commented out gross stuff

--- client/app.js
+++ client/app.js
@@ -2,7 +2,6 @@ import React, { Component } from 'react';
 import { Navbar, Heart, SpeechBubble, SelectDonke, PartyHat, HealthBar, Login, Playpen, Invitation, NewBuddy, Donke } from './components'
 import * as firebase from 'firebase' 
 import { connect } from 'react-redux'
-// import { updateAvatar } from './store'
 const secrets = require('../secrets.js')
 require("firebase/firestore")
 
@@ -24,49 +23,7 @@ db.settings(settings);
 export class App extends Component {
   constructor(props) {
     super(props)
-   // this.avatar = db.collection('avatars').doc(`${this.props.avatar.id}`)
-    // this.onUpdate = this.onUpdate.bind(this)
   }
-
-  // componentDidUpdate() {
-  //   //this works (see console for printout). You can update the avatar and you will get 
-  //   //a new snapshot. The doc id is just a sample from our database. 
-  //   // if (this.props.avatar.id) {
-  //   // this.unsubscribe = db.collection('avatars').doc(`${this.props.avatar.id}`).onSnapshot((doc)=> {
-  //   //   var source = doc.metadata.hasPendingWrites ? 'Local' : 'Server';
-  //   //   console.log(source, ' data: ', doc.data());
-  //   //   let updatedAvatar = doc.data()
-  //   //   this.onUpdate(updatedAvatar)
-  //   // });
-  //   // }
-  //   if (this.props.avatar.id){
-  //     db.collection('avatars').doc(`${this.props.avatar.id}`)
-  //       .get()
-  //       .then((returnedAvatar) => {
-  //         const firebaseAvatar = returnedAvatar.data()
-  //         this.unsubscribe = db.collection('avatars').doc(`${this.props.avatar.id}`).onSnapshot((snapshot) => {
-  //           if (this.props.avatar.invited !== firebaseAvatar.invited) {
-  //             let updatedAvatar = snapshot.data()
-  //             this.onUpdate(snapshot)
-  //           }
-  //         })
-  //       })
-  //   }
-  // }
-  // componentWillUnmount() {
-  //   this.unsubscribe();
-  // }
-
-  // onUpdate(updatedAvatar) {
-  //   console.log('SNAPSHOT', updatedAvatar)
-  //   this.props.updateAvatarStore(updatedAvatar)
-  // };
-
-//want their invitation to show up regardless of what stage they are in
-//want it to pop up on top of everything else
-//once they accept in a playpen, want the intervals to clear, and they set a new work/break interval for the playpen that applies to everyone
-//want it to show their current health but be on the same timer now
-//
 
   render() {
     return (
@@ -97,14 +54,5 @@ const mapStateToProps = state => {
     inPlaypen: state.playpenStatus
   }
 }
-
-
-// const mapDispatchToProps = dispatch => {
-//   return {
-//     updateAvatarStore(updatedAvatar) {
-//       dispatch(updateAvatar(updatedAvatar));
-//     }
-//   };
-// };
 
 export default connect(mapStateToProps)(App)
