6fd1d242af202400a0ae2d4d86f2aa1da7c2c39d
Mightynumnums <deadcookies@gmail.com>
2018-05-13 15:32:19 -0400

    toys

--- client/components/heart.js
+++ client/components/heart.js
@@ -1,7 +1,8 @@
 import React, { Component } from 'react';
 import { heartBeat } from '../library/animations'
+import { connect } from 'react-redux'
 
-export default class Heart extends Component {
+class Heart extends Component {
   constructor(props) {
     super(props);
   }
@@ -10,11 +11,26 @@ export default class Heart extends Component {
   }
 
   render() {
-
     return (
       <div>
-        <img id="heart" src="../img/heart.svg" width="30" height="30" />
+        {this.props.health >= 1
+          ?
+          <img id="heart" src="../img/heart.svg" width="30" height="30" />
+          :
+          <img id="brokenheart" src="../img/brokenheart.svg" width="30" height="30" />
+        }
       </div>
     );
   }
 }
+
+
+
+const mapStateToProps = state => {
+  return {
+    health: state.health,
+
+  }
+}
+
+export default connect(mapStateToProps)(Heart)
